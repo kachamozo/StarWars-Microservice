bcee24c51604b5554bd2403e7cc72b26a712dc34
kachamozo <cachamocito@yahoo.com>
2023-05-19 16:53:52 -0400

    add fn cuando existe el endpoint

--- characters/src/server.js
+++ characters/src/server.js
@@ -7,4 +7,9 @@ server.use(express.json());
 
 server.use('/characters', require('./routes'));
 
+// Esta parte es como un middleware cuando no existe el endPoint(chartersssq) nos direcciona aqui
+server.use('*', (req, res) => {
+	res.status(404).send('Not found');
+});
+
 module.exports = server;
