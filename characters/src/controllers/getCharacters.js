c5609ef5fbcf07ba92c880feb0791ef2346a361a
kachamozo <cachamocito@yahoo.com>
2023-09-20 16:57:41 -0400

    add response

--- characters/src/controllers/getCharacters.js
+++ characters/src/controllers/getCharacters.js
@@ -1,6 +1,7 @@
 const Characters = require('../data');
+const { response } = require('../utils');
 
 module.exports = async (req, res) => {
 	const characters = await Characters.list();
-	res.status(200).json(characters);
+	response(res, 200, characters);
 };
