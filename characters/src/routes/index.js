814b82e391dcda03392c047d8284323592eb9929
kachamozo <cachamocito@yahoo.com>
2023-09-21 16:21:05 -0400

    fix characters

--- characters/src/routes/index.js
+++ characters/src/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
-const characters = require('../controllers');
+const controllers = require('../controllers');
 const middleware = require('../middlewares');
 const router = Router();
 
 // solo importamos una sola ves el index de controllers y usamos como un objeto
 
-router.get('/', characters.getCharacters);
-router.post('/', middleware.caracterValidation, characters.createCharacter);
+router.get('/', controllers.getCharacters);
+router.post('/', middleware.caracterValidation, controllers.createCharacter);
 
 module.exports = router;
